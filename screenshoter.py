e701b76711530c72e90e44c90e0f8ec16aecbed4
kranewasd <kranehecaru@gmail.com>
2019-05-11 08:13:11 +0300

    Updated

--- screenshoter.py
+++ screenshoter.py
@@ -1,5 +1,3 @@
-#take screen shots but have a moving box on your desktop so you can resize it and take a screen shot there
-
 from PIL import ImageGrab, Image
 import pygame, sys, os
 from pygame.locals import *
@@ -10,54 +8,74 @@ from time import sleep
 import cv2
 import numpy as np
 
+#hide window
+import win32gui, win32con
+
+The_program_to_hide = win32gui.GetForegroundWindow()
+win32gui.ShowWindow(The_program_to_hide , win32con.SW_HIDE)
 #1 args since it needs to start systray
-def main(aa):
-	def takeSS():
-		img = ImageGrab.grab((0,0,1920,1080));
-		img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
-		cv2.imwrite("temp.jpg", img);
-		return img;
 
-	takeSS();
-	screenshot = Image.open("temp.jpg");
+def takeSS():
+	img = ImageGrab.grab((0,0,1920,1080));
+	img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+	cv2.imwrite("temp.jpg", img);
+	return img;
 
 
-
-	def saver(bbox):
+def saver(bbox):
 		imageToCrop = Image.open("temp.jpg");
 
 		startx, starty = bbox[0][0], bbox[0][1];
 		endx, endy = bbox[1][0], bbox[1][1];
 
+		#in case the user does it reverse for some reason
 
+		reverse1 = False
+		if startx >= endx:
+			endxNew = startx;
+			startx = endx;
+			reverse1 = True;
+		if starty >= endy:
+			endyNew = starty+1;
+			starty = endy+1;
+			reverse1 = True;
 
-		bbox = ((startx, starty, endx, endy));
+		
+
+		if reverse1:
+			bbox = ((startx, starty, endxNew, endyNew));
+		else:
+			bbox = ((startx, starty, endx, endy));
 		area = imageToCrop.crop(bbox)
 		area.save("screenShot.jpg");
 		imageToCrop.close();
 
+#draw lines
+storeValues = [];
+def liner(storeValues):
+	mC = mouse.get_position();
+	mCx,mCy = mC[0], mC[1];
 
-
-	#draw lines
-	storeValues = [];
-	def liner(storeValues):
-		mC = mouse.get_position();
-		mCx,mCy = mC[0], mC[1];
-
-		#select a point
-		if len(storeValues) != 2:
-			if keyboard.is_pressed("+"):
-				storeValues.append((mCx,mCy));
+	#select a point
+	if len(storeValues) != 2:
+		if keyboard.is_pressed("+"):
+			storeValues.append((mCx,mCy));
 		
-			for i in range(len(storeValues)-1):
-				if storeValues[i] == storeValues[i+1]:
-					storeValues.pop(i+1);
+		for i in range(len(storeValues)-1):
+			if storeValues[i] == storeValues[i+1]:
+				storeValues.pop(i+1);
 
+	#for coords in range(len(storeValues)-1):			
+		#pygame.draw.line(screen, (0,255,100), (storeValues[coords][0],storeValues[coords][1]), (storeValues[coords+1][0],storeValues[coords+1][1]), 2)
+	return storeValues;
 
-		#for coords in range(len(storeValues)-1):			
-			#pygame.draw.line(screen, (0,255,100), (storeValues[coords][0],storeValues[coords][1]), (storeValues[coords+1][0],storeValues[coords+1][1]), 2)
-		print(storeValues);
-		return storeValues;
+Running = True;
+def main():
+	global Running
+
+	takeSS();
+	screenshot = Image.open("temp.jpg");
+
 	pygame.init()
 
 	screen = pygame.display.set_mode((1920,1080), pygame.FULLSCREEN)
@@ -67,9 +85,6 @@ def main(aa):
 
 
 
-
-
-	Running = True
 	while Running:
 		for event in pygame.event.get():
 			if event.type == QUIT:
@@ -87,17 +102,18 @@ def main(aa):
 			saver(drawer);
 			screenshot.close();
 			os.system("del temp.jpg");
-			exit();
-
-		pygame.display.update()
+			Running = False;
+			pygame.quit();
+		if Running != False:
+			pygame.display.update()
 
 
 while 1:
 	sleep(1/100)
 	if keyboard.is_pressed("print screen"):
-		main(1);
+		Running = True;
+		storeValues = [];
+		main();
 	#exiting the program
 	if keyboard.is_pressed("ctrl") and keyboard.is_pressed("alt") and keyboard.is_pressed("+"):
 		exit();
-#menu_options = (("ScreenShot", None, main),)
-#systray = SysTrayIcon("icon.ico", "ScreenShot @krane", menu_options)
